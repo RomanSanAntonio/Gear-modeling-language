b0933a0a2a798a7a4647225bb0890329af68cfbc
Roman <roman.111.cool@gmail.com>
2025-06-01 08:59:20 -0400

    draft GML.cpp

---
+++ GML.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <regex>
+#include <cmath>
+
+using namespace std;
+
+struct Gear {
+  string name;
+  int teeth;
+  double
